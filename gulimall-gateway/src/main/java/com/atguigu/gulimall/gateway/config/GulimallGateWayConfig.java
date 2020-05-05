71d9019c81034572daaae9318b5618744ddfe595
ex-dingqi002 <381975263@qq.com>
2020-05-05 12:44:14 +0800

    'ok'

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallGateWayConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+public class GulimallGateWayConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", config);
+        CorsWebFilter corsWebFilter = new CorsWebFilter(urlBasedCorsConfigurationSource);
+        return corsWebFilter;
+    }
+}
