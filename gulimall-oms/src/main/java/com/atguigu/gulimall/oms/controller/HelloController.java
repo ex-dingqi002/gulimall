7d74479800ca5a6909df5c233d17021da92adec1
ex-dingqi002 <381975263@qq.com>
2020-05-23 15:55:50 +0800

    'ok'

--- gulimall-oms/src/main/java/com/atguigu/gulimall/oms/controller/HelloController.java
+++ gulimall-oms/src/main/java/com/atguigu/gulimall/oms/controller/HelloController.java
@@ -1,17 +1,24 @@
 package com.atguigu.gulimall.oms.controller;
 
 import com.atguigu.gulimall.oms.beproperties.beproperties;
+import com.atguigu.gulimall.oms.entity.OrderEntity;
 import com.atguigu.gulimall.oms.feign.WorldService;
+import com.atguigu.gulimall.oms.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RefreshScope
 @RestController
 public class HelloController {
     @Autowired
     WorldService worldService;
+
+    @Autowired
+    private OrderService orderService;
+
 
     @Value("${my.value}")
     String content;
@@ -20,10 +27,16 @@ public class HelloController {
     com.atguigu.gulimall.oms.beproperties.beproperties beproperties;
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
 
 
         String msg = worldService.world();
-        return "hello"+msg+content+""+beproperties.getLover();
+
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setMemberUsername("丁奇");
+        orderService.save(orderEntity);
+        return "hello" + msg + content + "" + beproperties.getLover();
+
+
     }
 }
--- gulimall-pms/src/main/java/com/atguigu/gulimall/pms/controller/WorldController.java
+++ gulimall-pms/src/main/java/com/atguigu/gulimall/pms/controller/WorldController.java
@@ -1,13 +1,24 @@
 package com.atguigu.gulimall.pms.controller;
 
+import com.atguigu.gulimall.pms.entity.BrandEntity;
+import com.atguigu.gulimall.pms.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorldController {
+
+    @Autowired
+    private BrandService brandService;
+
     @GetMapping("/world")
-    public String world(){
-        return  "world";
+    public String world() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setDescript("丁奇");
+        brandService.save(brandEntity);
+
+        return "world";
     }
 
 }
